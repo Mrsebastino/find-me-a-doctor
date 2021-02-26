2286aba66b039f7ad0ba7c8c057e843d52d8abe2
Sebastien Denommee <mrsebastino@gmail.com>
2021-02-26 16:35:52 +0000

    added functionality to retrieve doctor location. Not working yet

--- static/js/index.js
+++ static/js/index.js
@@ -1,10 +1,12 @@
-let map, infoWindow;
+let map, infoWindow, service;
 
 function initMap() {
   map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: -34.397, lng: 150.644 },
-    zoom: 10,
+    center: { lat: 	56.116600, lng: -3.936900},
+    zoom: 12,
   });
+
+
   infoWindow = new google.maps.InfoWindow();
   const locationButton = document.createElement("button");
   locationButton.textContent = "Pan to Current Location";
@@ -20,7 +22,7 @@ function initMap() {
             lng: position.coords.longitude,
           };
           infoWindow.setPosition(pos);
-          infoWindow.setContent("Location found.");
+          infoWindow.setContent("Your location found.");
           infoWindow.open(map);
           map.setCenter(pos);
           const marker = new google.maps.Marker({
@@ -30,14 +32,24 @@ function initMap() {
         },
         () => {
           handleLocationError(true, infoWindow, map.getCenter());
+
         }
       );
+
     } else {
       // Browser doesn't support Geolocation
       handleLocationError(false, infoWindow, map.getCenter());
     }
-  });
-}
+
+     infowindow = new google.maps.InfoWindow({map:map});
+            let service = new google.maps.places.PlacesService(map);
+            service.nearbySearch({
+                location: pos,
+                radius: 5000,
+                type: ['doctor, surgery']
+            }, callback);
+
+},
 
 function handleLocationError(browserHasGeolocation, infoWindow, pos) {
   infoWindow.setPosition(pos);
@@ -48,3 +60,25 @@ function handleLocationError(browserHasGeolocation, infoWindow, pos) {
   );
   infoWindow.open(map);
 }
+
+  )}
+function callback(results, status) {
+            if (status === google.maps.places.PlacesServiceStatus.OK) {
+                for (let i = 0; i < results.length; i++) {
+                    createMarker(results[i]);
+                }
+            }
+        }
+
+        function createMarker(place) {
+            let placeLoc = place.geometry.location;
+            let marker = new google.maps.Marker({
+                map: map,
+                position: place.geometry.location
+            });
+
+            google.maps.event.addListener(marker, 'click', function() {
+                infoWindow.setContent(place.name);
+                infoWindow.open(map, this);
+            });
+        }

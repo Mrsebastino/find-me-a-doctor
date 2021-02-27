6a9a6045b00f5aa2d33a2ca094c37e0c332d5766
Sebastien Denommee <mrsebastino@gmail.com>
2021-02-27 12:06:53 +0000

    user location and doctor nearby function implemented

--- static/js/index.js
+++ static/js/index.js
@@ -1,84 +1,101 @@
-let map, infoWindow, service;
+let map;
+let service;
+let infowindow;
+let pos;
+let request;
+let place;
 
 function initMap() {
-  map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: 	56.116600, lng: -3.936900},
-    zoom: 12,
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {
+      lat: -34.397,
+      lng: 150.644
+    },
+    zoom: 6
   });
+  infoWindow = new google.maps.InfoWindow;
 
+  getLocation();
+  // getNearByPlaces();
+  // callback();
+}
 
-  infoWindow = new google.maps.InfoWindow();
-  const locationButton = document.createElement("button");
-  locationButton.textContent = "Pan to Current Location";
-  locationButton.classList.add("custom-map-control-button");
-  map.controls[google.maps.ControlPosition.TOP_CENTER].push(locationButton);
-  locationButton.addEventListener("click", () => {
-    // Try HTML5 geolocation.
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(
-        (position) => {
-          const pos = {
-            lat: position.coords.latitude,
-            lng: position.coords.longitude,
-          };
-          infoWindow.setPosition(pos);
-          infoWindow.setContent("Your location found.");
-          infoWindow.open(map);
-          map.setCenter(pos);
-          const marker = new google.maps.Marker({
-              position: pos,
-              map: map,
-  });
-        },
-        () => {
-          handleLocationError(true, infoWindow, map.getCenter());
+function getLocation() {
+  // Try HTML5 geolocation.
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      pos = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+      };
+      console.log("getLocation:" + pos.lat + "," + pos.lng);
+      let marker = new google.maps.Marker({
+        position: pos,
+        map: map,
+        icon: "http://maps.google.com/mapfiles/ms/micons/blue.png"
+      })
+      infoWindow.setPosition(pos);
+      infoWindow.setContent('Location found.');
+      infoWindow.open(map);
+      map.setCenter(pos);
+      getNearByPlaces(pos);
+    }, function() {
+      console.log("calling handleLocationError(true)");
+      handleLocationError(true, infoWindow, map.getCenter());
+    });
+  } else {
+    // Browser doesn't support Geolocation
+    console.log("calling handleLocationError(false)")
+    handleLocationError(false, infoWindow, map.getCenter());
+  }
 
-        }
-      );
+  infowindow = new google.maps.InfoWindow();
+}
 
-    } else {
-      // Browser doesn't support Geolocation
-      handleLocationError(false, infoWindow, map.getCenter());
+function getNearByPlaces(pos) {
+  console.log("getNearByPlaces:" + pos.lat + "," + pos.lng);
+  request = {
+    location: pos,
+    radius: '5000',
+    query: 'doctor'
+  };
+
+  service = new google.maps.places.PlacesService(map);
+  service.textSearch(request, callback);
+}
+
+function callback(results, status) {
+  if (status == google.maps.places.PlacesServiceStatus.OK) {
+    console.log("callback received " + results.length + " results");
+    let bounds = new google.maps.LatLngBounds();
+    for (let i = 0; i < results.length; i++) {
+      console.log(JSON.stringify(results[i]));
+      place = results[i];
+      let mark = createMarker(results[i]);
+      bounds.extend(mark.getPosition());
     }
-
-     infowindow = new google.maps.InfoWindow({map:map});
-            let service = new google.maps.places.PlacesService(map);
-            service.nearbySearch({
-                location: pos,
-                radius: 5000,
-                type: ['doctor, surgery']
-            }, callback);
-
-},
+    map.fitBounds(bounds);
+  } else console.log("callback.status=" + status);
+}
 
 function handleLocationError(browserHasGeolocation, infoWindow, pos) {
   infoWindow.setPosition(pos);
-  infoWindow.setContent(
-    browserHasGeolocation
-      ? "Error: The Geolocation service failed."
-      : "Error: Your browser doesn't support geolocation."
-  );
+  infoWindow.setContent(browserHasGeolocation ?
+    'Error: The Geolocation service failed.' :
+    'Error: Your browser doesn\'t support geolocation.');
   infoWindow.open(map);
 }
 
-  )}
-function callback(results, status) {
-            if (status === google.maps.places.PlacesServiceStatus.OK) {
-                for (let i = 0; i < results.length; i++) {
-                    createMarker(results[i]);
-                }
-            }
-        }
+function createMarker(place) {
+  let marker = new google.maps.Marker({
+    map: map,
+    position: place.geometry.location,
+    icon: "http://maps.google.com/mapfiles/ms/micons/red.png"
+  });
 
-        function createMarker(place) {
-            let placeLoc = place.geometry.location;
-            let marker = new google.maps.Marker({
-                map: map,
-                position: place.geometry.location
-            });
-
-            google.maps.event.addListener(marker, 'click', function() {
-                infoWindow.setContent(place.name);
-                infoWindow.open(map, this);
-            });
-        }
+  google.maps.event.addListener(marker, 'click', function() {
+    infowindow.setContent(place.name);
+    infowindow.open(map, this);
+  });
+  return marker;
+}

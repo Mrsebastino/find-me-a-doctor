2457689bedab1985442f8d820ba9f95188ea3464
Sebastien Denommee <mrsebastino@gmail.com>
2021-03-03 11:36:00 +0000

    added comment to code

--- app.py
+++ app.py
@@ -9,16 +9,19 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/home')
 def home():
+    """Return the home page"""
     return render_template("index.html")
 
 
 @app.route('/booking_form')
 def booking_form():
+    """Return the booking form page"""
     return render_template("booking_form.html")
 
 
 @app.route('/appointment_booked', methods=["GET", "POST"])
 def appointment_booked():
+    """Load data from the form and returned confirmation to user"""
     if request.method == "POST":
         req = request.form
         fullname = req.get("fullname")
